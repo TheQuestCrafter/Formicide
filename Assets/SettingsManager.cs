629f718cb951303962deabc6c70bdeb4dd2b2344
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2019-12-23 00:02:08 -0600

    Settings Manager Started

---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour
+{
+    private Scene CurrentScene;
+
+    public float volume;
+    public int screenHeight;
+    public int screenWidth;
+    public bool fullscreen;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        CurrentScene = SceneManager.GetActiveScene();
+    }
+
+    void Update()
+    {
+        CheckSceneMatch();
+    }
+
+    private void CheckSceneMatch()
+    {
+        if(CurrentScene != SceneManager.GetActiveScene())
+        {
+            ApplySettings();
+        }
+        CurrentScene = SceneManager.GetActiveScene();
+    }
+
+    private void ApplySettings()
+    {
+        Screen.fullScreen = fullscreen;
+
+    }
+}
